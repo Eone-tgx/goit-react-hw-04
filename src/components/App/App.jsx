49da169a6e3a0246ecf367891479d407c425c60d
Eone-tgx <eone1024@gmail.com>
2025-04-12 00:16:52 +0300

    react-hw-4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,33 +1,91 @@
+import Modal from "react-modal";
 import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import SearchBar from "../SearchBar/SearchBar";
+import { Toaster } from "react-hot-toast";
+import ImageGallery from "../ImageGallery/ImageGallery";
+import { fetchData } from "../../api-requests";
+import { ClipLoader } from "react-spinners";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "../LoadMoreBtn/LoadMoreBtn";
+import ImageModal from "../ImageModal/ImageModal";
+
+Modal.setAppElement("#root");
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [query, setQuery] = useState("");
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleSearch = async (query) => {
+    setImages([]);
+    setQuery(query);
+    setPage(1);
+    setError(null);
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetchData(query, 1);
+      setImages(response.results);
+      setTotalPages(response.total_pages);
+    } catch (error) {
+      setError("Something went wrong. Please try again.");
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const loadMoreImages = async () => {
+    setPage(page + 1);
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetchData(query, page + 1);
+      setImages((prev) => [...prev, ...response.results]);
+      requestAnimationFrame(() => {
+        window.scrollBy({
+          top: window.innerHeight / 0.9,
+          behavior: "smooth",
+        });
+      });
+    } catch (error) {
+      setError("Something went wrong. Please try again.");
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const openModal = (image) => setSelectedImage(image);
+  const closeModal = () => {
+    setSelectedImage(null);
+    document.body.classList.remove("ReactModal__Body--open");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SearchBar onSubmit={handleSearch} />
+
+      <Toaster position="top-right" reverseOrder={false} />
+
+      <ImageGallery images={images} onImageClick={openModal} />
+
+      {isLoading && <ClipLoader color="#646cff" size={50} />}
+
+      {error && <ErrorMessage />}
+
+      {images.length > 0 && page < totalPages && !isLoading && (
+        <LoadMoreBtn onClick={loadMoreImages} />
+      )}
+
+      {selectedImage && (
+        <ImageModal image={selectedImage} onClose={closeModal} />
+      )}
     </>
   );
 }
