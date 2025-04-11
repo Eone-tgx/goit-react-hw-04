bf13dcc2ebdeed2412c69e1cdb0cf42a8b1bae7d
Eone-tgx <eone1024@gmail.com>
2025-04-12 00:22:39 +0300

    react-hw-4

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -49,7 +49,7 @@ function App() {
       setImages((prev) => [...prev, ...response.results]);
       requestAnimationFrame(() => {
         window.scrollBy({
-          top: window.innerHeight / 1.2,
+          top: window.innerHeight / 1.3,
           behavior: "smooth",
         });
       });
